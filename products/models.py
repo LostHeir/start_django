498b26d142acda6edf24267fc3fd99d885f2371b
Mateusz Strzelecki <mateuszstrzelecki21@gmail.com>
2021-10-17 20:33:50 +0200

    change Product model.

--- products/models.py
+++ products/models.py
@@ -3,7 +3,11 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='some stuff')
+    title = models.CharField(max_length=120)  # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField()
+
+    # new feature added after DB was created.
+    featured = models.BooleanField()  # options: null=True -> All previous items can be empty, default=True
+    # default value can be set also after migration
